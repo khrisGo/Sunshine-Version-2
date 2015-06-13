5608b1c8634154622ac89754034fe241a1b998d6
khrisGo <khris.gomri@gmail.com>
2015-06-14 01:50:44 +0200

    Implémentation d'une liste météo factice

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -53,7 +57,36 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            //On récupère la vue conteneur
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //On génére les données factices
+            ArrayList<String> weatherList = new ArrayList<>();
+            weatherList.add("Today - Sunny - 88/63");
+            weatherList.add("Tomorrow - Foggy - 70/46");
+            weatherList.add("Weds - Cloudy - 72/63");
+            weatherList.add("Thurs- Rainy - 64/51");
+            weatherList.add("Fri - Foggy - 70/46");
+            weatherList.add("Saturday - Sunny - 76/68");
+            weatherList.add("Sunday - Sunny - 88/63");
+
+            //On crée un Adapter qui va servir à indiquer comment chaque élément doit être affiché.
+            //L'Adapter récupère une référence vers les données brutes (Paramètre 4)
+            //L'Adapter récupère la façon dont chaque donnée doit être affichée :
+            //Où la donnée doit être affichée ? Dans le layout list_item_forecast (Paramètre 2)
+            //Comment ? Selon les indications dans le TextView list_item_forecast_textview (Paramètre 3)
+            ArrayAdapter<String> weatherAdapter;
+            weatherAdapter = new ArrayAdapter<String>(getActivity(),
+                                                      R.layout.list_item_forecast,
+                                                      R.id.list_item_forecast_textview,
+                                                      weatherList);
+
+            //On récupère la référence vers la ListView
+            ListView listViewFC = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+            //On lie la ListView à l'Adapter
+            listViewFC.setAdapter(weatherAdapter);
+
             return rootView;
         }
     }
